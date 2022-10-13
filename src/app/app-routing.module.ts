ef072a0d9edcc7f2ab1bce74cb28ffe2667ecbef
Jabreal Johnson <jabrealthecoder@gmail.org>
2022-10-13 14:01:29 -0400

    add support for github actions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ResumeComponent} from './resume/resume.component';
+import {EngineeringComponent} from './engineering/engineering.component';
+import {WorkshopsComponent} from './workshops/workshops.component';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'resume', component: ResumeComponent},
+  {path: 'engineering', component: EngineeringComponent},
+  {path: 'workshops', component: WorkshopsComponent},
+  {path: '', component: HomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+export const RoutingComponents = [HomeComponent, EngineeringComponent, ResumeComponent, WorkshopsComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { EngineeringComponent } from './engineering/engineering.component';
+import { ResumeComponent } from './resume/resume.component';
+import { WorkshopsComponent } from './workshops/workshops.component';
+import { HomeComponent } from './home/home.component';
+import {HttpClientModule} from "@angular/common/http";
+import {NgxTypedJsModule} from "ngx-typed-js";
+import {DownloadFileService} from "./download-file.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    RoutingComponents,
+    AppComponent,
+    EngineeringComponent,
+    ResumeComponent,
+    WorkshopsComponent,
+    HomeComponent
   ],
   imports: [
+    HttpClientModule,
+    NgxTypedJsModule,
+    FontAwesomeModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+
   ],
-  providers: [],
+  providers: [DownloadFileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
