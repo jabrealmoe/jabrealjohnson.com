925981dc07722996c226a2a6ff4ea809ccd133f9
Jabreal Johnson <jabrealthecoder@gmail.org>
2022-10-14 13:25:26 -0400

    add bunch of changes

--- src/app/engineering/engineering.component.ts
+++ src/app/engineering/engineering.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as THREE from "three";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 @Component({
   selector: 'app-engineering',
@@ -10,6 +12,65 @@ export class EngineeringComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(74, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGL1Renderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+    const material = new THREE.MeshStandardMaterial({ color: 0x233a77});
+    const torus = new THREE.Mesh( geometry, material)
+    scene.add(torus);
+
+    const pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(20, 20, 20);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    scene.add(pointLight, ambientLight);
+
+    const lightHelper = new THREE.PointLightHelper(pointLight)
+    const gridHelper = new THREE.GridHelper(200, 50);
+    scene.add(lightHelper, gridHelper);
+
+    const controls = new OrbitControls(camera, renderer.domElement)
+
+    function addStar(){
+      const geometry = new THREE.SphereGeometry(0.25, 24, 24)
+      const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+      const star = new THREE.Mesh(geometry, material);
+
+      const vertices = []
+      for ( let i = 0; i < 10000; i ++ ) {
+
+        const x = THREE.MathUtils.randFloatSpread( 2000 );
+        const y = THREE.MathUtils.randFloatSpread( 2000 );
+        const z = THREE.MathUtils.randFloatSpread( 2000 );
+
+        vertices.push( x, y, z );
+
+      }
+
+      const spaceTexture = new THREE.TextureLoader().load('space.png');
+      scene.background = spaceTexture;
+      star.position.set(vertices[0], vertices[1], vertices[3])
+      scene.add(star)
+      Array(200).fill(1, 25).forEach(addStar)
+    }
+
+
+
+    camera.position.setZ(30);
+    const animate = function () {
+      requestAnimationFrame(animate);
+      torus.rotation.x += 0.01;
+      torus.rotation.y += 0.005;
+      torus.rotation.z += 0.01;
+      controls.update();
+
+      renderer.render(scene, camera);
+    };
+    animate();
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as THREE from 'three';
 
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,25 +13,10 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(74, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGL1Renderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00});
-    const cube = new THREE.Mesh( geometry, material)
-    scene.add(cube);
-    camera.position.z = 5;
-    const animate = function () {
-      requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
-      renderer.render(scene, camera);
-    };
-    animate();
   }
 
+  goToBlogs() {
+
+  }
 }
